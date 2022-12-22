819c63d901831ea4829999fb3c3c466ba2bb40fa
Dariy_Ershova <d-r-2015@mail.ru>
2022-12-23 01:18:54 +0500

    3Fixation

---
+++ Work3/dom3.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+int gump= new Random().Next(1,8);
+Console.WriteLine(gump);
+if ((gump==6)||(gump==7))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    
+    Console.WriteLine("No");
+}
